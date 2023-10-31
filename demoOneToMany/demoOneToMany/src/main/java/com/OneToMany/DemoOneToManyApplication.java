e81f9906f14ac49fe99e83031da80064c2c51d03
vijaystha <102886940+vijaystha@users.noreply.github.com>
2023-10-30 23:54:06 -0400

    Add files via upload

---
+++ demoOneToMany/demoOneToMany/src/main/java/com/OneToMany/DemoOneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.OneToMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOneToManyApplication.class, args);
+	}
+
+}
---
+++ demoOneToMany/demoOneToMany/src/test/java/com/OneToMany/DemoOneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OneToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
